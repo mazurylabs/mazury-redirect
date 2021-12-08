5cb415d8c11d080398f671a2695c3c75af183f7a
wojtekwtf <wojciechkulikowski@icloud.com>
2021-12-08 22:07:53 +0100

    Add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <PlausibleProvider domain="mzry.me">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
